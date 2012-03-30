f06506014f7d42e06d71a237c45e9ac358a16d57
rimoer <rimoer@qq.com>
2012-03-30 22:43:00 +0800

    ok

--- MyBatisGenerator/script/db_script.sql
+++ MyBatisGenerator/script/db_script.sql
@@ -18,20 +18,20 @@ CREATE TABLE  `t_employee` (
   PRIMARY KEY (`id`,`id_parent`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE  `t_type` (
+CREATE TABLE  `t_dict` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-  `code_type` varchar(3) NOT NULL,
+  `code_dict` varchar(3) NOT NULL,
   `describe` varchar(255) NOT NULL,
   `sort_order` int(11) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE  `t_type_value` (
+CREATE TABLE  `t_dict_value` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `id_parent` bigint(20) NOT NULL,
-  `code_type` varchar(3) NOT NULL,
-  `name_type` varchar(4) NOT NULL,
-  `value_type` varchar(8) NOT NULL,
+  `code_dict` varchar(3) NOT NULL,
+  `name_dict` varchar(4) NOT NULL,
+  `value_dict` varchar(8) NOT NULL,
   `describe` varchar(255) NOT NULL,
   `sort_order` int(11) NOT NULL,
   PRIMARY KEY (`id`,`id_parent`)
