5a5a555b2f2f69e414cb283e8811e561b31d1cc8
rimoer <rimoer@qq.com>
2012-04-01 23:47:41 +0800

    ok

--- wyz.component.util/src/main/java/com/wyz/component/util/ConfigUtils.java
+++ wyz.component.util/src/main/java/com/wyz/component/util/ConfigUtils.java
@@ -2,7 +2,12 @@ package com.wyz.component.util;
 
 import java.io.File;
 
+import org.apache.commons.configuration.ConfigurationException;
 import org.apache.commons.configuration.PropertiesConfiguration;
+import org.apache.commons.configuration.XMLPropertiesConfiguration;
+import org.apache.commons.configuration.event.ConfigurationEvent;
+import org.apache.commons.configuration.event.ConfigurationListener;
+import org.apache.log4j.PropertyConfigurator;
 
 /**
  * @author wangyz
@@ -12,21 +17,39 @@ import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class ConfigUtils {
 
-	public static PropertiesConfiguration propConfig = null;
+	public static PropertiesConfiguration config = new PropertiesConfiguration();
+	static {
+		config.addConfigurationListener(new ConfigurationListener() {
+			public void configurationChanged(ConfigurationEvent event) {
+				try {
+					config.refresh();
+				} catch (ConfigurationException e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
-	public static void loadConfig(String path) {
+	public static void loadConfig(String path) throws ConfigurationException {
 		File file = new File(path);
 		if (file.exists() && file.isFile()) {
-			
+			if (file.getPath().endsWith(".xml")) {
+				config.append(new XMLPropertiesConfiguration(file));
+			} else {
+				config.append(new PropertiesConfiguration(file));
+			}
+		}
+	}
+
+	public static void loadLog4j(String path) {
+		File file = new File(path);
+		if (file.exists() && file.isFile()) {
+			PropertyConfigurator.configureAndWatch(path, 60 * 60 * 1000);
 		}
 	}
 
 	public static PropertiesConfiguration getConfig() {
-		if (null == propConfig) {
-
-		}
-
-		return propConfig;
+		return config;
 	}
 
 }
