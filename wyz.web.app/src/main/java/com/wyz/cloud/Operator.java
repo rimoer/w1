1ca479c50c993a708cb9506e2e6cc4e9d7822606
rimoer <rimoer@qq.com>
2012-04-01 21:23:45 +0800

    ok

---
+++ wyz.web.app/src/main/java/com/wyz/cloud/Operator.java
@@ -0,0 +1,13 @@
+package com.wyz.cloud;
+
+/**
+ * @author wangyz
+ * @since 2012-3-28 下午11:23:02
+ * @version 1.0
+ */
+
+public interface Operator {
+	void process();
+
+	Object process(Object obj);
+}
