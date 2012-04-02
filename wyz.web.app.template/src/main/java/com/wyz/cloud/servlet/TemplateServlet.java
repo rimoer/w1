814b4da80c225b4446de4d9ada0a39c10693938c
rimoer <rimoer@qq.com>
2012-04-02 17:01:35 +0800

    ok

--- wyz.web.app.template/src/main/java/com/wyz/cloud/servlet/TemplateServlet.java
+++ wyz.web.app.template/src/main/java/com/wyz/cloud/servlet/TemplateServlet.java
@@ -2,6 +2,7 @@ package com.wyz.cloud.servlet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.wyz.cloud.AbstractOperator;
 import com.wyz.service.template.TemplateService;
@@ -16,6 +17,7 @@ public class TemplateServlet extends AbstractOperator {
 	private static final long serialVersionUID = -3509693766765853957L;
 	private final static Logger log = LoggerFactory.getLogger(TemplateServlet.class);
 
+	@Autowired
 	TemplateService templateService = null;
 
 	@Override
